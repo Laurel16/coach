bf1e5cbdb3a217dd05c2f8e4544a28516f0e04e6
Laure Leter <laure.leter@gmail.com>
2019-07-08 19:36:56 +0200

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/dashboard', to: 'users#dashboard'
+
+  post '/user/edit', to: 'users#update'
   devise_for :users
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
