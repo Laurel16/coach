9d1559d8263296af3cdd149ebf6d4d8fd3e4a613
Laure Leter <laure.leter@gmail.com>
2020-03-12 19:03:47 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'requests/index'
-  get 'requests/new'
-  get 'requests/create'
-  get 'requests/edit'
-  get 'requests/update'
-  get 'requests/show'
-  get 'requests/destroy'
-  get 'requests/list'
+
   root 'pages#home'
   get '/dashboard', to: 'users#dashboard'
   get '/users/:id', to: 'users#show'
   get '/selling_orders', to: 'orders#selling_orders'
   get '/buying_orders', to: 'orders#buying_orders'
+  get '/all-requests', to: 'requests#list'
 
   post '/users/edit', to: 'users#update'
 
@@ -26,6 +20,8 @@ Rails.application.routes.draw do
 
     resources :orders, only: [:create]
   end
+
+  resources :requests
 
   devise_for :users,
               path: '',
